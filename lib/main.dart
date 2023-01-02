da342e4ebb6b0940232cbc19aac346a8bea87e21
Dawood <dododawood@gmail.com>
2023-01-02 17:52:35 +0200

    add Localization

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,46 @@
 import 'package:flutter/material.dart';
 import 'package:news/layout/home.dart';
+import 'package:news/provider/my_provider.dart';
+import 'package:news/screens/details/details.dart';
 import 'package:news/screens/setting.dart';
 import 'package:news/shared/styles/my_theme.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:provider/provider.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 void main() {
-  runApp( MyApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => MyProvider(),
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
+    var provider = Provider.of<MyProvider>(context);
     return MaterialApp(
+
+      localizationsDelegates: [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: [
+        Locale('en' ), // English, no country code
+        Locale('ar' ), // Arabic, no country code
+      ],
+      locale: Locale(provider.language),
       initialRoute: HomeLayout.routeName,
-       routes: {
-        SettingScreen.routeName : (context) => SettingScreen(),
-        HomeLayout.routeName : (context) => HomeLayout(),
-       },
+      routes: {
+        SettingScreen.routeName: (context) => SettingScreen(),
+        HomeLayout.routeName: (context) => HomeLayout(),
+        NewsDetailsScreen.routeName: (context) => NewsDetailsScreen(),
+      },
       debugShowCheckedModeBanner: false,
     );
   }
 }
-
-
---
+++ lib/shared/constants/constants.dart
@@ -0,0 +1,2 @@
+const String APIKEY = "69c37ac46f7b4156b0e7a4d89e9416aa";
+const String BASE = 'newsapi.org';
